e3999a68b3ec31adfde94ae701317d74dd569243
Sudheep24 <sudheeps2004@gmail.com>
2024-02-27 19:38:45 +0530

    middleware

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      clerkId: id,
+      clerkId: id!,
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
@@ -82,7 +82,7 @@ export async function POST(req: Request) {
     const {id,email_addresses, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      clerkID:id,
+      clerkID:id!,
       email:email_addresses[0].email_address,
       firstName: first_name,
       lastName: last_name,
